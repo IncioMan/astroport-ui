10ccf5b88004fb1e7b6ee1b5a3bbb012abf06e4e
Alex Incerti <a.incerti@iconsulting.biz>
2022-04-05 15:44:38 +0200

    input numbers

--- src/components/AssetToSwap/AssetToSwap.js
+++ src/components/AssetToSwap/AssetToSwap.js
@@ -8,7 +8,7 @@ export default function AssetToSwap(props) {
             <div className={className}>
                 <p>{asset}</p>
                 <div className='asset-container'>
-                    {(owned && <input onChange={onChange} tabindex="2" className='amount-input' placeholder="0" type="text" />)}    
+                    {(owned && <input onChange={onChange} tabindex="2" className='amount-input' placeholder="0" type="number" />)}    
                     {(!owned && <div className='amount-calculated'>{amount}</div>)}   
                     <img className='asset-logo' src={logo} width="35" alt="Italian Trulli"></img>
                 </div>
