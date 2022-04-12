7fd963bcada5a695db03ef894908b77163ca72e0
Alex Incerti <a.incerti@iconsulting.biz>
2022-04-12 12:16:22 +0200

    latest pairs

--- src/components/SwapSuggestionList/SwapSuggestionList.js
+++ src/components/SwapSuggestionList/SwapSuggestionList.js
@@ -5,15 +5,28 @@ import './SwapSuggestionList.css'
 import SwapContext from '../SwapContainer/SwapContext';
 import pools from '../../data/astroport.dex.js'
 import tokens from '../../data/tokens.js'
+const axios = require('axios').default;
 
 export default function SwapSuggestionList(props) {
-    const {title, pairs} = props
+    const {title} = props
     const {setSwapValue} = useContext(SwapContext);
     const [loaded, setLoaded] = useState(false);
+    const [pairs, setPairs] = useState([]);
 
     useEffect(()=>{
-        console.log()
-        setTimeout(()=>setLoaded(true), 5000)
+        axios.get('https://api.flipsidecrypto.com/api/v2/queries/786bfe99-df83-4285-adb0-834db5101b0e/data/latest')
+        .then(function (response) {
+            // handle success
+            setPairs(response.data.map((p)=>p["POOL_ADDRESS"]).slice(0,4))
+            setLoaded(true)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
     })
 
     return (
@@ -28,7 +41,6 @@ export default function SwapSuggestionList(props) {
                 const pool = pools.mainnet[pair]
                 const assetFrom = tokens.mainnet[pool.assets[0]]
                 const assetTo = tokens.mainnet[pool.assets[1]]
-                console.log(pair, assetFrom, assetTo)
                 return <SwapSuggestion 
                     key={pair}
                     asset1={assetFrom.symbol}
