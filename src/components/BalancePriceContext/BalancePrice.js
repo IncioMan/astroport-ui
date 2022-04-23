49e2461fe5fc8ca5785b66047797525273c11b63
Alex Incerti <alex.incerti@outlook.com>
2022-04-23 06:40:28 +0200

    balance works

--- src/components/BalancePriceContext/BalancePrice.js
+++ src/components/BalancePriceContext/BalancePrice.js
@@ -5,58 +5,67 @@ import { useConnectedWallet, useLCDClient, useWallet, WalletStatus} from '@terra
 const axios = require('axios').default;
 import tokens from '../../data/tokens.js'
 
-function setBalancesReducer(state, value) {
+function setBalancePriceReducer(state, value) {
     if(!value){
         return state
     }
     const newState = {};
     Object.assign(newState, state);
     for(let token of Object.keys(value)){
-        newState[token] = value[token]
+        if(!Object.keys(newState).includes(token)){
+            newState[token] = {}
+        }
+        if("balance" in value[token]){
+            newState[token].balance = value[token].balance
+        }
+        if("price" in value[token]){
+            newState[token].price = value[token].price
+        }
     }
     return newState
 }
 
-function setPricesReducer(state, value) {
-    if(!value){
-        return state
-    }
-    const newState = {};
-    Object.assign(newState, state);
-    for(let token of Object.keys(value)){
-        newState[token] = value[token]
-    }
-    return newState
-}
 
 export default function BalancePrice(props) {
     const {swapValue, setSwapValue} = useContext(SwapContext);
     const lcd = useLCDClient();
     const connectedWallet = useConnectedWallet();
     const {status} = useWallet();
-    const [balances, setBalances] = useReducer(setBalancesReducer, {});
-    const [prices, setPrices] = useReducer(setPricesReducer, {});
+    const [BalancePrice, setBalancePrice] = useReducer(setBalancePriceReducer, {});
     
     useEffect(() => {
-        if(!connectedWallet){
-            return []
-        }
         const tokensToFetch = [swapValue.assetFrom.asset, swapValue.assetTo.asset,
             'terra1hj8de24c3yqvcsv9r8chr03fzwsak3hgd8gv3m',
             'terra1xfsdgcemqwxp4hhnyk4rle6wr22sseq7j07dnn',
             'terra12hgwnpupflfpuual532wgrxu2gjp0tcagzgx4n']
         for (let token of tokensToFetch) {
+            if(!connectedWallet){
+                const value = {}
+                value[token] = {price: null, balance: null}
+                setBalancePrice(value)
+                continue
+            }
             const tokenInfo = tokens.mainnet[token]
+            const value = {}
+            value[token] = {price:'loading', balance:'loading'}
+            setBalancePrice(value)
             axios.get('https://api.extraterrestrial.money/v1/api/prices?symbol='+tokenInfo.symbol)
             .then(function (response) {
                 if(tokenInfo.symbol  in response.data.prices){
                     const value = {}
-                    value[token] = response.data.prices[tokenInfo.symbol].price
-                    setPrices(value)
+                    value[token] = {price: response.data.prices[tokenInfo.symbol].price}
+                    setBalancePrice(value)
+                }else{
+                    const value = {}
+                    value[token] = {price: null}
+                    setBalancePrice(value)
                 }
             })
             .catch(function (error) {
                 console.log(error);
+                const value = {}
+                value[token] = {price: null}
+                setBalancePrice(value)
             })
             .then(function () {
                 // always executed
@@ -64,13 +73,19 @@ export default function BalancePrice(props) {
             if(['uusd','uluna'].includes(token)){
                 lcd.bank.balance(connectedWallet.walletAddress).then(([coins]) => {
                     let value = {}
+                    value[token] = {}
                     if(token in coins['_coins']){
-                        value[token]= coins['_coins'][token].amount/1000000
+                        value[token].balance = coins['_coins'][token].amount/1000000
                     }
                     else{
-                        value[token] = 0
+                        value[token].balance = 0
                     }
-                    setBalances(value)
+                    setBalancePrice(value)
+                }).catch(function (error) {
+                    console.log(error);
+                    const value = {}
+                    value[token] = {balance: null}
+                    setBalancePrice(value)
                 })
             }
             else{
@@ -80,8 +95,14 @@ export default function BalancePrice(props) {
                     })
                     .then((res)=>{
                         const value = {}
-                        value[token] = res.balance/1000000
-                        setBalances(value)
+                        value[token] = {}
+                        value[token].balance = res.balance/1000000
+                        setBalancePrice(value)
+                    }).catch(function (error) {
+                        console.log(error);
+                        const value = {}
+                        value[token] = {balance: null}
+                        setBalancePrice(value)
                     })
             }
         }
